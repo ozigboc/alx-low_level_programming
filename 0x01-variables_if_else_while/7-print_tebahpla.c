9830dfa2b495757da58646140d0edbe1cf2a18be
ozigboc <ozigboc@gmail.com>
2022-04-09 14:37:23 +0100

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 /**
- *  * main - prints out all the numbers of base
- *   * 16 in lowercase
+ *  * main - main block
+ *   * Description: prints out the alphabet in reverse
  *    * Return: 0
  *     */
 int main(void)
 {
-		char c;
-			int i;
+		char a = 'z';
 
-				for (i = 0; i < 10; i++)
-							putchar(i + '0');
-					for (c = 'a'; c <= 'f'; c++)
-								putchar(c);
-						putchar('\n');
-							return (0);
+			while (a >= 'a')
+					{
+								putchar(a);
+										a--;
+											}
+
+				putchar('\n');
+					return (0);
 }
