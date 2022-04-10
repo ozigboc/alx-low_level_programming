8732da1249e2969805d882312d8e6e4ad2507d0c
ozigboc <ozigboc@gmail.com>
2022-04-10 20:48:45 +0100

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  *     */
 int main(void)
 {
-		int i = 0;
+	int i = 0;
 
-			while (i < 10)
-					{
-								putchar(i + '0');
-										if (i < 9)
-													{
-																	putchar(44);
-																				putchar(32);
-																						}
-												i++;
-													}
-				putchar('\n');
-					return (0);
+	while (i < 10)
+	{
+	putchar(i + '0');
+	if (i < 9)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	i++;
+	}
+	putchar('\n');
+	  return (0);
 }
