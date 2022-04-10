54466d36aba5d87958edefa23ff5b84b7c294ad4
ozigboc <ozigboc@gmail.com>
2022-04-10 10:00:15 +0100

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,13 +23,13 @@ int main(void)
 
 	else if (n < 0)
 	{	
-        printf("%d is negative\n", n);
-	}
+        printf("%d is negative\n", n);		
+       	}		
 
 	else
 	{
 	printf("%d is zero\n", n);
 	}
 	return (0);
-	
+							
 }
