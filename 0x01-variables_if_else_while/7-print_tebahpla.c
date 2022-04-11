383da6ff9540d21940df064d39fc973d77a2c564
ozigboc <ozigboc@gmail.com>
2022-04-11 16:40:04 +0100

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,17 +4,20 @@
  *  * main - main block
  *   * Description: prints out the alphabet in reverse
  *    * Return: 0
- *     */
-  int main(void)
+      */
+int main(void)
 {
-		
-    char a = 'z';
-    while (a >= 'a')
-    {
-    putchar(a);
-    a--;
+
+	char a = 'z';
+
+	while (a >= 'a')
+
+	{
+		putchar(a);
+		a--;
 			}
-                     
-	putchar('\n');
-       return (0);
+                   
+      	putchar('\n');
+      
+       	return (0);
 }
