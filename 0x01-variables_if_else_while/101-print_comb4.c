6d09bd9fb45fddc7b7ed2d23a3149354dcba6b51
ozigboc <ozigboc@gmail.com>
2022-04-11 23:14:23 +0100

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,11 +15,11 @@
 
 int main(void)
 {
-       	int i, j, k, l;
-	
-	for (i = 0; i < 1000; i++)
-	
-	{
+int i, j, k, l;
+
+for (i = 0; i < 1000; i++)
+
+{
 		j = i / 100; /*hundreds*/
 		k = (i / 10) % 10; /*tens*/
 		l = i % 10; /*singles*/
