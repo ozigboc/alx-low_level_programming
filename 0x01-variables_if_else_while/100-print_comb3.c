21ed3ac24261b318373a610aa33048f982ba177c
ozigboc <ozigboc@gmail.com>
2022-04-11 17:22:12 +0100

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,32 +10,32 @@
  *         * You can only use `putchar` up to 5 times.
  *          * You are not allowed to use any variable of type `char`.
  *           * Return: 0
- *            */
-int main(void)
-{
-	int i, j, k;
+             */
 
+int main(void)
+
+{
+
+	int i, j, k;
 	i = 0;
 
 	while (i < 100)
+{
+	j = i % 10;/*singles digit*/
+	k = i / 10;/*doubles digit*/
+	if (k < j)
 	{
-		j = i % 10;/*singles digit*/
-		k = i / 10;/*doubles digit*/
-		if (k < j)
+		putchar(k + '0');
+		putchar(j + '0');
+		if (i < 89)
 		{
-			putchar(k + '0');
-			putchar(j + '0');
-
-			if (i < 89)
-			{
-				putchar(44);
-				putchar(32);
-			}
+			putchar(44);
+			putchar(32);
 		}
-
-		i++;
 	}
-	putchar('\n');
-
-	return (0);
+	i++;
+}
+putchar('\n');
+	
+return (0);
 }
