c8f7b5953481a127935799de4f91e908da239b58
ozigboc <ozigboc@gmail.com>
2022-04-11 17:53:12 +0100

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,30 +19,30 @@ int main(void)
 {
 	int i, j;
 	int a, b, c, d;
-	
+
 	for (i = 0; i < 100; i++)
-	
+
 	{
 		a = i / 10; /*doubles fnum*/
 		b = i % 10; /*singles fnum*/
-		
+
 		for (j = 0; j < 100; j++)
 		
 		{
 			c = j / 10; /*doubles snum*/
 			d = j % 10; /*singles snum*/
-			
+
 			if (a < c || (a == c && b < d))
-			
+
 			{
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
-				
+
 				if (!(a == 9 && b == 8))
-				
+			
 				{
 					putchar(44);
 					putchar(32);
@@ -53,4 +53,4 @@ int main(void)
 		}
 		putchar(10);
 		return (0);
-	}
+}
