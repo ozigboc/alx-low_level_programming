d2a92bb74346da914c631ac096b4d0cd84e1758d
ozigboc <ozigboc@gmail.com>
2022-04-12 22:47:44 +0100

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,22 +4,22 @@
  *  * print_alphabet_x10 - print alphabet 10 times
  *   *
  *    * Return: Always 0
- *     */
+      */
 void print_alphabet_x10(void)
 {
-		int k, go;
+	int k, go;
 
-			go = 0;
+	go = 0;
 
-				while (go < 10)
-						{
-									for (k = 'a'; k <= 'z'; k++)
-												{
-																_putchar(k);
-																		}
-											go++;
+	while (go < 10)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+		{
+			_putchar(k);
+		}
+		go++;
 
-													_putchar('\n');
-														}
+		_putchar('\n');
+	}
 
 }
