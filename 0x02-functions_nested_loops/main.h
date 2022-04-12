e40fa99c33499563f9672c8ffc13d77e55442387
ozigboc <ozigboc@gmail.com>
2022-04-13 00:31:59 +0100

    task 11

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int print_sign(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-it add(int, int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif
