4e3da65adb6520a77e2c2467dfd1dcac0f34c61a
ozigboc <ozigboc@gmail.com>
2022-04-12 22:38:56 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,16 @@
  *   * Description: this program prints out the alphabet in lowercase
  *    *
  *     * Return: Always 0.
-      */
+       */
 void print_alphabet(void)
 {
-		char text = 'a';
+	char text = 'a';
 
-		while (text <= 'z')
-					{
-						_putchar(text);
-						text++;
-					}
+	while (text <= 'z')
+	{
+		_putchar(text);
+		text++;
+	}
 
-				_putchar('\n');
+	_putchar('\n');
 }
