538492cdde24ba597b0583c1a871b201a42b2caf
ozigboc <ozigboc@gmail.com>
2022-04-12 22:42:58 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,14 @@
 
 int main(void)
 {
-		int i;
-			char msg[9] = "_putchar";
-			
-			for (i = 0; i < 8; i++)
+	int i;
+	char msg[9] = "_putchar";
 
-						{
-									_putchar(msg[i]);
-										}
-					_putchar('\n');
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(msg[i]);
+	}
+	_putchar('\n');
 
-						return (0);
+	return (0);
 }
