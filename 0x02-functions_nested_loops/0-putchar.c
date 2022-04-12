52294c7ee0db2d2845996bd035debc80b7bbb2e6
ozigboc <ozigboc@gmail.com>
2022-04-12 21:24:12 +0100

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,18 @@
  *   * Description: this program prints _putchar without using
  *    * the printf function.
  *     * Return: 0
-       */
+ *      */
 
 int main(void)
 {
-	int i;
-	char msg[9] = "_putchar";
-	
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(msg[i]);
-		
-		_putchar('\n');
-		return (0);
-	
-	}
-	
-	}
+		int i;
+			char msg[9] = "_putchar";
+
+				for (i = 0; i < 8; i++)
+						{
+									_putchar(msg[i]);
+										}
+					_putchar('\n');
+
+						return (0);
+}
