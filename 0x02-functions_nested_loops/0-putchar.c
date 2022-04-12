9a63362955be9ebe2b2a06fe1bb63c4e4197c022
ozigboc <ozigboc@gmail.com>
2022-04-12 20:17:59 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
 #include "main.h"
-/** main - main block
- * Description: create a program to print _putschar followed by new line
- * Return 0
- */
+
+/**
+ *  * main - main block
+ *   * Description: this program prints _putchar without using
+ *    * the printf function.
+ *     * Return: 0
+       */
+
 int main(void)
 {
-printf("_putschar");
-
-return(0);
-
-}
+	int i;
+	char msg[9] = "_putchar";
+	
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(msg[i]);
+		
+		_putchar('\n');
+		
+		return (0);
+	}
