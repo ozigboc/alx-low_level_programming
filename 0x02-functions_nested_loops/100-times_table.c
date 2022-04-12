d3b624b2feedf9e9db86ae536bde3d16ef9c6771
ozigboc <ozigboc@gmail.com>
2022-04-13 00:49:24 +0100

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
 /**
  * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
- * Description: prints the times table    
+ * Description: print times table    
  * Return: void
  */
 
