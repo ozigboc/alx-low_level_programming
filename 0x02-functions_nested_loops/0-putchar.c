05261a17c20cceb86591cb5dec1f6fae8848c159
ozigboc <ozigboc@gmail.com>
2022-04-12 18:02:54 +0100

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** main - main block
+ * Description: create a program to print _putschar followed by new line
+ * Return 0
+ */
+int main(void)
+{
+printf("_putschar");
+
+return(0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My nested loop readme.md.
