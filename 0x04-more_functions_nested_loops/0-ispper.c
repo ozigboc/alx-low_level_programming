101bf274b0a85b5338af7071653a81883705a90f
ozigboc <ozigboc@gmail.com>
2022-04-14 23:54:04 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-ispper.c
+++ 0x04-more_functions_nested_loops/0-ispper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  * _isupper - checks for uppercase character
- *   * @c: the character to be checked
+ *    * _isupper - checks for uppercase character
+ *    * @c: the character to be checked
  *    * Return: 1 if c is uppercase, otherwise 0
       */
 int _isupper(int c)
