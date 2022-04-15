94bda4c0e8056ca8769dc09e5c7e107b94faf1bc
ozigboc <ozigboc@gmail.com>
2022-04-15 07:45:14 +0100

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * print_line - a function thta drwas a straight line in the terminal.
- *   * followed by a newline
- *    * @n: an input integer
- *     * Return: Always 0.
-        */
+ * * print_line - a function thta drwas a straight line in the terminal.
+ * * followed by a newline
+ * * @n: an input integer
+ * * Return: Always 0.
+   */
 void print_line(int n)
 {
 	int i = 0;
