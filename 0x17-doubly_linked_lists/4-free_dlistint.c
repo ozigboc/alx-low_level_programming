2f09d25696c064963003f026bb015c3c08321b2e
ozigboc <ozigboc@gmail.com>
2022-07-17 21:45:38 +0200

    linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * * free_dlistint - frees a doubly linked list
+ * * @head: head of node
+ * * Return: 0 on success
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
