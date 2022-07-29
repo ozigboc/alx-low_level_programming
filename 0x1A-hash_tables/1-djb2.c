09f2da7be3eb8aafd53f0f505c4c5db78a3bbe0d
ozigboc <ozigboc@gmail.com>
2022-07-29 22:46:17 +0800

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,7 +4,7 @@
  * * hash_djb2 - hash function generates a djb2
  * * @str: pointer to chars a hash
  * * Return: hash key
- * */
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long hash = 5381;
